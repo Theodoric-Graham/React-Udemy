b1466e42e2d14cd9f34023bf75494b98a5c16a57
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-07-21 17:57:14 -0500

    Introducing JSX

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom/client';
+//index.js is the first file to be executed here
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//createRoot creates the main entry point
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//call the render method to say what should be rendered, in this case App
+//JSX(Javascript XML) syntax, App is a component, that will be rendered on the root
 root.render(<App />);
