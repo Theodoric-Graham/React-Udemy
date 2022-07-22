7828cd84264e3797e06695201c86dca395e83eb4
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-07-21 19:32:14 -0500

    Building our first custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+//our own custom components must start with a capital letter when in JSX so
+//react can detect it as a custom component
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
