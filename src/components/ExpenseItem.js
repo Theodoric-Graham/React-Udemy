e0d9ef28873652be43ea5a4cbaa436533ed00b34
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-07-24 15:39:49 -0500

    Adding basic CSS styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   //you can only have one root element here per return statement, per JSX code snippet
   return (
-    <div>
+    <div className="expense-item">
       <div>July 24th, 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <div className="expense-item__price">$294.67</div>
       </div>
     </div>
   );
