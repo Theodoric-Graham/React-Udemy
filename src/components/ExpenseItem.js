04af0e76b6dd087fe5e71f84b106d3b381e29be6
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-07-24 16:22:58 -0500

    Outputting Dynamic Data & Working with Expressions in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 6, 24);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
   //you can only have one root element here per return statement, per JSX code snippet
   return (
     <div className="expense-item">
-      <div>July 24th, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
