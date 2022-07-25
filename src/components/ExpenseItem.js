f3147b77f8f6d0eead8e4ddb0bfac2ca142d7e3a
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-07-25 00:24:09 -0500

    adding "normal" JS Logic to Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,18 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   console.log(props);
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   //you can only have one root element here per return statement, per JSX code snippet
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
