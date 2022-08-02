2199cc8afef54a9ed315b0029d674a4fce66bd90
Theodoric-Graham <theodoricgraham@hotmail.com>
2022-08-01 23:24:34 -0500

    Listening to events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,9 +4,13 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ index, date, title, amount }) => {
+  const clickHandler = () => {
+    console.log("clicked!!");
+  };
   const oddEven = `expense-item ${index % 2 === 0 ? "even" : "odd"}`;
 
   //you can only have one root element here per return statement, per JSX code snippet
+  //we dont need to call 'clickHandler' we just need to point at it, regardless of how its defined
   return (
     <Card className={oddEven}>
       <ExpenseDate date={date} />
@@ -14,6 +18,8 @@ const ExpenseItem = ({ index, date, title, amount }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
