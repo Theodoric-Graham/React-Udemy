6f25d1a0ca918221446c0f1e3aa67d8db56c6eb1
Theodoric-Graham <theodoricgraham@hotmail.com>
2023-09-12 23:59:27 -0500

    added line

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -46,6 +46,7 @@ export default function App() {
             <div className={step >= 1 ? "active" : ""}>1</div>
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
           <p className="message">
