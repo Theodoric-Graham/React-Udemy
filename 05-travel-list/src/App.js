4ac06a87a3fd4162b0d250b93471e62d649b254d
Theodoric-Graham <theodoricgraham@hotmail.com>
2023-11-21 23:22:56 -0600

    added more items to list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -6,6 +6,10 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: true },
+  { id: 3, description: "Laptop", quantity: 1, packed: true },
+  { id: 3, description: "Wallet", quantity: 1, packed: false },
+  { id: 3, description: "Keys", quantity: 1, packed: false },
+  { id: 3, description: "Phone", quantity: 1, packed: true },
 ];
 
 export default function App() {
