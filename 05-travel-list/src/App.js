8961afafea629245913f2a679f0ff947046a0ccc
Theodoric-Graham <theodoricgraham@hotmail.com>
2023-12-04 23:14:27 -0600

    Update App.js

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -6,27 +6,27 @@ const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: true },
-  { id: 4, description: "Laptop", quantity: 1, packed: true },
-  { id: 5, description: "Wallet", quantity: 1, packed: false },
-  { id: 6, description: "Keys", quantity: 1, packed: false },
-  { id: 7, description: "Phone", quantity: 1, packed: true },
-  { id: 8, description: "Mouse", quantity: 1, packed: true },
-  { id: 9, description: "Keyboard", quantity: 1, packed: true },
-  { id: 10, description: "Mouse Pad", quantity: 1, packed: true },
+  // { id: 4, description: "Laptop", quantity: 1, packed: true },
+  // { id: 5, description: "Wallet", quantity: 1, packed: false },
+  // { id: 6, description: "Keys", quantity: 1, packed: false },
+  // { id: 7, description: "Phone", quantity: 1, packed: true },
+  // { id: 8, description: "Mouse", quantity: 1, packed: true },
+  // { id: 9, description: "Keyboard", quantity: 1, packed: true },
+  // { id: 10, description: "Mouse Pad", quantity: 1, packed: true },
 ];
 
 export default function App() {
   const [items, setItems] = useState([]);
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
   //spread existing items and add new one
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
 
-  function handleAddData(data) {
-    setItems((newData) => [...newData, data]);
-  }
+  // function handleAddData(data) {
+  //   setItems((newData) => [...newData, data]);
+  // }
 
   return (
     <div className="app">
