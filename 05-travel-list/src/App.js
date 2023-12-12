8315330f2e4c29d00f475f8d84ab4d9a89201869
Theodoric-Graham <theodoricgraham@hotmail.com>
2023-12-11 23:14:42 -0600

    Update App.js

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -34,6 +34,7 @@ export default function App() {
       <Form onAddItems={handleAddItems} />
       <PackingList items={items} />
       <NewList />
+      <SecondList />
       <Stats />
     </div>
   );
