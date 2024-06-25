4bba1568918d7cf31375fb9dc86b80b9abd86310
Theodoric-Graham <theodoricgraham@hotmail.com>
2024-06-25 16:35:07 -0500

    More reusability with the "children" prop

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -10,6 +10,14 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <StepMessage step={1}>
+        <p>Pass in Content</p>
+        <p>👍</p>
+      </StepMessage>
+      <StepMessage step={2}>
+        <p>Pass in Content</p>
+        <p>😽</p>
+      </StepMessage>
       {/* <Steps /> */}
     </div>
   );
@@ -60,10 +68,18 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-            {/* {test.name} */}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                bgColor="#e7e7e7"
+                textColor="#333"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn How
+              </Button>
+            </div>
+          </StepMessage>
           <div className="buttons">
             <Button
               bgColor={"#7950f2"}
@@ -74,10 +90,20 @@ function Steps() {
             </Button>
             <Button bgColor={"#7950f2"} textColor={"#fff"} onClick={handleNext}>
               Next <span>👉</span>
+              <span>🤓</span>
             </Button>
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
