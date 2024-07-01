6c632d871b34c28e5061f3b190b2e2187166d737
Theodoric-Graham <theodoricgraham@hotmail.com>
2024-07-01 17:09:31 -0500

    Setting up components

--- tip-calc-challenge/tip-calculator/src/App.js
+++ tip-calc-challenge/tip-calculator/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Bill />
+      <Service />
+      <FriendService />
+      <Total />
+    </div>
+  );
+}
+
+function Bill() {
+  return (
+    <div>
+      <p>How much was the bill?</p>
+      <input></input>
+    </div>
+  );
+}
+
+function Service() {
+  return (
+    <div>
+      <p>How did you like the service?</p>
+      <label for="service-select"></label>
+      <select name="pets" id="service-select">
+        <option value="dissatisfied">Dissatisfied (0%)</option>
+        <option value="okay">It was okay (5%)</option>
+        <option value="good">It was good (10%)</option>
+        <option value="amazing">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function FriendService() {
+  return (
+    <div>
+      <p>How did your friend like the service?</p>
+      <label for="service-select"></label>
+      <select name="pets" id="service-select">
+        <option value="dissatisfied">Dissatisfied (0%)</option>
+        <option value="okay">It was okay (5%)</option>
+        <option value="good">It was good (10%)</option>
+        <option value="amazing">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Total() {
+  return (
+    <div>
+      <h2>You pay $x</h2>
     </div>
   );
 }
